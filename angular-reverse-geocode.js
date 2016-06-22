186fd4e210edf63d27d0296006359704b2eede6d
Lawrence Stone <larry@ibeat.com>
2016-06-22 13:23:44 -0700

    fixing component syntax error and updating demo.

--- angular-reverse-geocode.js
+++ angular-reverse-geocode.js
@@ -5,37 +5,35 @@
  * @version 2.0.0
  */
 (function () {
-    angular.module('angularReverseGeocode', []).component('reverseGeocode', function () {
-        return {
-            bindings: {
-              lat: '@',
-              lng: '@',
-              geocodeResultsIndex: '@?',
-              locationNotFoundText: '@?',
-              geocodeFailureText: '@?'
-            },
-            template: '<span class="reverse-geocoded-address">{{$ctrl.address}}</span>',
-            controller: function () {
-                var vm = this;
-                vm.address = '';
-                var defaultResultsIndex = 0;
-                var defaultLocationNotFoundText = 'Location not found';
-                var defaultGeocodeFailureText = 'Geocoder failed due to: ';
-                var geocoder = new google.maps.Geocoder();
-                var latlng = new google.maps.LatLng(vm.lat, vm.lng);
+    angular.module('angularReverseGeocode', []).component('reverseGeocode', {
+        bindings: {
+            lat: '@',
+            lng: '@',
+            geocodeResultsIndex: '@?',
+            locationNotFoundText: '@?',
+            geocodeFailureText: '@?'
+        },
+        template: '<span class="reverse-geocoded-address">{{$ctrl.address}}</span>',
+        controller: function () {
+            var vm = this;
+            vm.address = '';
+            var defaultResultsIndex = 0;
+            var defaultLocationNotFoundText = 'Location not found';
+            var defaultGeocodeFailureText = 'Geocoder failed due to: ';
+            var geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(vm.lat, vm.lng);
 
-                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        if (results[defaultResultsIndex]) {
-                            vm.address = results[vm.geocodeResultsIndex || defaultResultsIndex].formatted_address;
-                        } else {
-                            vm.address = vm.locationNotFoundText || defaultLocationNotFoundText
-                        }
+            geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    if (results[defaultResultsIndex]) {
+                        vm.address = results[vm.geocodeResultsIndex || defaultResultsIndex].formatted_address;
                     } else {
-                        vm.address = vm.geocodeFailureText || (defaultGeocodeFailureText + status);
+                        vm.address = vm.locationNotFoundText || defaultLocationNotFoundText
                     }
-                });
-            }
+                } else {
+                    vm.address = vm.geocodeFailureText || (defaultGeocodeFailureText + status);
+                }
+            });
         }
     });
 })();
